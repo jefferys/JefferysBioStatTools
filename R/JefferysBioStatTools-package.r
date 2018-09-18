7c2d9af5f8929a5e46ff3976e65bb521035279bb
Stuart R. Jefferys <srj@unc.edu>
2018-09-18 14:16:44 +0000

    DEV: Initial commit.

---
+++ R/JefferysBioStatTools-package.r
@@ -0,0 +1,13 @@
+#' JefferysBioStatTools - Biostatistic tools organized for easy use
+#'
+#' Statistical tools are scattered all over the place in R. I want one place
+#' to find them. I also want \emph{my} documentation for them to go somewhere
+#' directly accessible, not hidden in text files. I expect most things here
+#' will be wrappers around existing functions. It does add overhead to pass big
+#' data, have to be careful about that.
+#'
+#' @name JefferysBioStatTools
+#' @docType package
+#'
+NULL
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(JefferysBioStatTools)
+
+test_check("JefferysBioStatTools")
